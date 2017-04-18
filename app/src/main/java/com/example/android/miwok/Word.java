f560a18e0c75e107dc452b0679ba64985e7b80e2
tamiromara <tamiromara@gmail.com>
2017-04-19 02:01:55 +0400

    EOD

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,8 +1,35 @@
 package com.example.android.miwok;
 
 /**
- * Created by Elomara on 4/19/17.
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for the word.
  */
 
 public class Word {
+
+    /** Default translation for the word*/
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    // Constructor
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translation of the word
+     * */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word
+     * */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
 }
